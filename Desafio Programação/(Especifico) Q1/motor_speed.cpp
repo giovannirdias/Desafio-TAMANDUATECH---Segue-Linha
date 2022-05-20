5ef7eefad88ae677961541d9595c8863e674523c
Giovanni <99917909+giovannirdias@users.noreply.github.com>
2022-05-20 11:13:44 -0300

    Add files via upload

---
+++ Desafio Programação/(Especifico) Q1/motor_speed.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "PID.h"
+
+int base_speed = 200;
+int min_speed = -255;
+int max_speed = 255;
+
+void new_motor_speed(){
+    int left_motor_speed = base_speed - PID::getAjuste();
+    int right_motor_speed = base_speed + PID::getAjuste();
+
+    // Verificar se o motor esta dentro dos valores limites
+    if (left_motor_speed > max_speed){
+        left_motor_speed = max_speed;
+    }
+    if (left_motor_speed < min_speed){
+        left_motor_speed = min_speed;
+    }
+    if (right_motor_speed > max_speed){
+        left_motor_speed = max_speed;
+    }
+    if (right_motor_speed < min_speed){
+        left_motor_speed = min_speed;
+    }
+
+    // Funcao para alterar a velocidade do motor atraves dos sinais PWM
+    set_motor_speed(right_motor_speed, left_motor_speed);
+}
